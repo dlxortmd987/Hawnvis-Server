1422a1d68263da20bc8df1ab4e990736ef0053bd
dlxortmd987 <dlxortmd987@google.com>
2024-11-06 14:07:17 +0900

    initial commit

---
+++ src/main/java/project/hwanvis/hawnvisserver/HawnvisServerApplication.java
@@ -0,0 +1,13 @@
+package project.hwanvis.hawnvisserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawnvisServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HawnvisServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/hwanvis/hawnvisserver/HawnvisServerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.hwanvis.hawnvisserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawnvisServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
